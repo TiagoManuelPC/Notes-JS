835bf24cb5ca621ba061c61c4f43c5a88dcd48ce
Abdur Rehman <wabdulrehman@yahoo.com>
2021-05-12 13:20:33 +0100

    create a li tag for abbreviated note with it's own id

--- public/js/src/interface.js
+++ public/js/src/interface.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // function updateNotes() {
   //   document.querySelector('#notes').innerText = notepad.showNotes()
   //    };
-
+  var index = 0
   const notepad = new NotePad();
 
   document.querySelector("#submit-button").addEventListener('click', function(){
@@ -11,8 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
     notepad.add(text)
     let abbText = notepad.display()
     let li = document.createElement("li")
-    li.innerText = abbText
+    li.innerHTML = `<a id="${index}" >${abbText}</a>`
+    index ++ 
     document.querySelector("#display").append(li)
     document.querySelector("#create-note").value = ''
   })
+
+
 })
