f2926aa37793f329fa85e15c3d6bec1b3482e471
TiagoManuelPC <tiagomanuelpereiracampos@hotmail.com>
2021-05-16 02:32:53 +0100

    wip

--- public/js/src/interface.js
+++ public/js/src/interface.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  // function updateNotes() {
-  //   document.querySelector('#notes').innerText = notepad.showNotes()
-  //    };
-  var index = 0
-  var tester = false
+  
   const notepad = new NotePad();
 
+  //------------------------------------
+  // gets the emoji api
   createEmoji = async (text, callback) => {
+    const obj = { text: text}
     let jsonText = JSON.stringify(`{ "text": "${text}" }`)
+    //let jsonText = `{ "text": "${text}" }`
     await fetch("https://makers-emojify.herokuapp.com/", {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -18,53 +17,102 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(data => callback(data.emojified_text))
   }
 
-  function displayAllNotes(){
-    document.querySelector("ul").innerHTML = ""
-    for(let i = 0; i < notepad.notes.length; i++){
-      let abbText = notepad.notes[i].abbreviate()
-      createEmoji(abbText, (abbText)=> {
-      let li = document.createElement("li")
-      li.setAttribute("id", `m${i}`)
-      li.innerHTML = `<a href="#${i}">${abbText}</a>` 
-      document.querySelector("#display").append(li)
-      })
-    }
-  }
 
+  document.getElementById("create-note").addEventListener('click', function(){
+    showBox();
+  })
   
-
-  document.querySelector("#submit-button").addEventListener('click', function(){
-    let text = document.querySelector("#create-note").value
-    
-
-    if(text ===''){
-      return
-    }
-    
-    notepad.add(text)
-    
-    displayAllNotes()
-    
-    document.querySelector("#create-note").value = ''
-    
+  document.getElementById("textInput").addEventListener("keyup", function(event){
+    if (event.key != "Enter") {
+    return}
+    saveNote()
   })
 
-  document
-    .querySelector("#display")
-    .querySelectorAll("li")
-    .addEventListener('click', urlChange())
-    
-    function urlChange() {
-      window.addEventListener("hashchange", () => {
-        displayAllNotes()
-        let index_page = `#m${location.href.split("#")[1]}`
 
-        document
-        .querySelector(index_page)
-        .innerHTML = notepad.notes[location.href.split("#")[1]].text;
+  document.querySelector("#submit-note").addEventListener('click', function(){
+    saveNote()
+  })
+
+  
+  
+  // function displayAllNotes(){
+  //   document.querySelector("ul").innerHTML = ""
+  //   for(let i = 0; i < notepad.notes.length; i++){
+  //     let abbText = notepad.notes[i].abbreviate()
+  //     let li = document.createElement("li")
+  //     li.setAttribute("id", `${i}`) 
+  //     createEmoji(abbText, (abbText)=> {
+  //     li.innerHTML = `<a href="#${i}">${abbText}</a>` 
+  //     })
+  //     document.querySelector("#abbreviated-list").append(li)
+  //   }
+  // }
+ 
+   document.getElementById('abbreviated-list').addEventListener('click', function(){
+     displayAllNote()
+   })
+
+   document.querySelector("#returnallnotes").addEventListener("click", function(){
+    document.querySelector("ol").classList.remove("hidden")
+    document.getElementById("fullNote").classList.add("hidden")
+   })
+
+   
+  
+  function displayAllNote() {
+    document.addEventListener("hashchange", () => {
+    let index_page = `#m${location.href.split("#")[1]}`
+    document
+    .querySelector(index_page)
+    .innerText = notepad.notes[location.href.split("#")[1]].text;
     });
   };
+  function displayNewNote(text){
+    var index = notepad.notes.length
+      let abbText = notepad.notes[index-1].abbreviate()
+      let li = document.createElement("li")
+      li.setAttribute("id", `note${index-1}`) 
+      createEmoji(abbText, (abbText)=> {
+      li.innerHTML = `<a href="#${index}">${abbText}</a>` 
+      })
+      document.querySelector("#abbreviated-list").append(li)
+      document.querySelector(`#note${index-1}`).addEventListener("click", function(e){
+        console.log(e.target)
+      var index = e.target.parentElement.id.replace("note", "") 
+      var fullText = notepad.notes[index].text
+      createEmoji(fullText, (fullText)=>{
+        document.getElementById("alltext").innerHTML = fullText
+      })
+      document.querySelector("ol").classList.add("hidden")
+      document.getElementById("fullNote").classList.remove("hidden")
+      document.getElementById("alltext").innerHTML = fullText
+      })
+      
+    }
+
+  function saveNote(){
+    let text = document.querySelector("#textInput").value.replace("\n", "")
+    notepad.add(text)
+    displayNewNote(text)
+    document.querySelector("#textInput").value = ''
+    hideBox()
+    console.log(notepad.notes)
+  }
+  
+  
+
+  function showBox() {
+    document.getElementById("save-note").style.display = "block"
+    document.getElementById("textInput").focus()
+  }
+
+  function hideBox() {
+    document.getElementById("save-note").style.display = "none"
+  }
 
     
 })
 
+  // document.getElementById("abbreviated-list")
+  // .querySelectorAll("li")
+  // .addEventListener('click', urlChange())
